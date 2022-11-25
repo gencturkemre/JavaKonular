e1a73b5dcfb11e0330f3e05367e7a3b9c5c870a7
gencturkemre <you@example.com>
2022-11-25 15:56:25 +0300

    day44

---
+++ src/ders44_maps/MapDepo.java
@@ -0,0 +1,63 @@
+package ders44_maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDepo {
+
+    // ogrenci map olusturup bize donduren bir method olusturun
+
+    public static Map<Integer, String> ornekMapOlustur() {
+        Map<Integer, String> ogrenciMap = new HashMap<>();
+        ogrenciMap.put(101, "Ali-Can-10-H-MF");
+        ogrenciMap.put(102, "Veli-Cem-11-M-Soz");
+        ogrenciMap.put(103, "Ali-Cem-11-H-TM");
+        ogrenciMap.put(104, "Ayse-Can-10-H-MF");
+        ogrenciMap.put(105, "Ayse-Cem-11-M-TM");
+        ogrenciMap.put(106, "Fatma-Han-10-K-Soz");
+
+        return ogrenciMap;
+    }
+
+    public static String isimSoyisimDondur(Map<Integer, String> ogrenciMap, int ogrenciKey) {
+       /*
+           {
+           101=Ali-Can-10-H-MF,
+           102=Veli-Cem-11-M-Soz,
+           103=Ali-Cem-11-H-TM,
+           104=Ayse-Can-10-H-MF,
+           105=Ayse-Cem-11-M-TM,
+           106=Fatma-Han-10-K-Soz}
+           ogrenciKey=105
+         */
+
+        String ogrenciValue = ogrenciMap.get(ogrenciKey); // "Ayse-Cem-11-M-TM"
+
+        String[] valueArr = ogrenciValue.split("-"); // [Ayse, Cem, 11, M, TM]
+
+        String isimSoyisim = valueArr[0] + " " + valueArr[1]; // Ayse Cem
+
+        return isimSoyisim;
+    }
+
+    public static Map<Integer, String> ogrenciYeniSubeAta(Map<Integer, String> ogrenciMap, int ogrenciKey, String yenisube) {
+        if (ogrenciMap.containsKey(ogrenciKey)) {
+
+            String istenenKeyinValuesu = ogrenciMap.get(ogrenciKey);
+            String[] istenenKeyinValueArr = istenenKeyinValuesu.split("-");
+            istenenKeyinValueArr[3] = yenisube;
+            String istenenKeyinYeniValesu = istenenKeyinValueArr[0] + "-" +
+                    istenenKeyinValueArr[1] + "-" +
+                    istenenKeyinValueArr[2] + "-" +
+                    istenenKeyinValueArr[3] + "-" +
+                    istenenKeyinValueArr[4];
+
+
+                    ogrenciMap.put(ogrenciKey,istenenKeyinYeniValesu );
+
+
+        }
+            return ogrenciMap;
+
+    }
+}
